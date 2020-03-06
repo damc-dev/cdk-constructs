f7e2c9ccd81c84b17d3069c136bb9a4b4b5d22e2
David McElligott <david.mcelligott@mutualofomaha.com>
2020-03-05 22:16:52 -0600

    added tests for billing alerts

---
+++ lib/billing_alert.ts
@@ -0,0 +1,63 @@
+import * as cdk from '@aws-cdk/core';
+import * as cw_actions from '@aws-cdk/aws-cloudwatch-actions';
+import * as sns from '@aws-cdk/aws-sns';
+import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
+
+import { Alarm, Metric, ComparisonOperator } from '@aws-cdk/aws-cloudwatch';
+
+export interface BillingAlertProps {
+    /**
+     * The threshold of estimated charges in dollars to alert on
+     * @default = 1
+     */
+    readonly alarmThreshold?: number;
+
+    /**
+     * Email addresses to send alerts to
+     * @default = []
+     */
+    readonly emailSubscriptions?: string[];
+}
+
+export class BillingAlert extends cdk.Construct {
+    /**
+     * SNS topic alerts get published to
+     */
+    readonly topic: sns.Topic;
+
+    /**
+     * Billing alarm
+     */
+    readonly alarm: Alarm;
+
+    constructor(scope: cdk.Construct, id: string, props: BillingAlertProps = {}) {
+        super(scope, id);
+
+        this.topic = new sns.Topic(this, 'BillingTopic');
+
+        const estimatedCharges = new Metric({
+            namespace: 'AWS/Billing',
+            metricName: 'EstimatedCharges',
+            dimensions: {
+                Currency: 'USD'
+            }
+        });
+
+        this.alarm = new Alarm(this, 'BillingAlarm', {
+            alarmDescription: 'Alert when estimated charges exceed threshold',
+            metric: estimatedCharges,
+            threshold: props.alarmThreshold || 1,
+            comparisonOperator: ComparisonOperator.GREATER_THAN_THRESHOLD,
+            evaluationPeriods: 1,
+            statistic: 'max'
+        });
+
+        this.alarm.addAlarmAction(new cw_actions.SnsAction(this.topic));
+
+        if(props.emailSubscriptions !== undefined && props.emailSubscriptions.length > 0) { 
+            props.emailSubscriptions.forEach((email) => {
+                this.topic.addSubscription(new subscriptions.EmailSubscription(email));
+            });
+        }
+    }
+}
---
+++ test/billing_alert.test.ts
@@ -0,0 +1,75 @@
+import { expect as expectCDK, haveResource, SynthUtils, countResources } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import { BillingAlert } from '../lib/billing_alert';
+
+test('BillingAlert created with no email subscriptions', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+
+  new BillingAlert(stack, 'MyBillingAlert');
+
+  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::CloudWatch::Alarm"));
+  
+  expectCDK(stack).to(countResources("AWS::SNS::Subscription", 0));
+});
+
+test('BillingAlert created with email subscriptions', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+
+  new BillingAlert(stack, 'MyBillingAlert', {
+    emailSubscriptions: [
+      'test1@example.com',
+      'test2@example.com'
+    ]
+  });
+
+  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::CloudWatch::Alarm", {
+    Threshold: 1
+  }));
+  expectCDK(stack).to(haveResource("AWS::SNS::Subscription", {
+    Protocol: "email",
+    Endpoint: "test1@example.com"
+  }));
+  expectCDK(stack).to(haveResource("AWS::SNS::Subscription", {
+    Protocol: "email",
+    Endpoint: "test2@example.com"
+  }));
+});
+
+test('BillingAlert created with threshold', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+
+  new BillingAlert(stack, 'MyBillingAlert', {
+    alarmThreshold: 5
+  });
+
+  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::CloudWatch::Alarm", {
+    Threshold: 5
+  }));
+});
+
+test('BillingAlert created with subscriptions and threshold', () => {
+  const app = new cdk.App();
+  const stack = new cdk.Stack(app, "TestStack");
+  
+  new BillingAlert(stack, 'MyBillingAlert', {
+    alarmThreshold: 5,
+    emailSubscriptions: [
+      'test@example.com'
+    ]
+  });
+
+  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::CloudWatch::Alarm", {
+    Threshold: 5
+  }));
+  expectCDK(stack).to(haveResource("AWS::SNS::Subscription", {
+    Protocol: "email",
+    Endpoint: "test@example.com"
+  }));
+});
